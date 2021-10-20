8328c61766d86f50ecbf01e7d732e8aca8348474
luhc228 <luhengchang228@126.com>
2021-10-20 23:30:52 +0800

    feat: add commitlint

--- packages/spec/src/index.js
+++ packages/spec/src/index.js
@@ -2,10 +2,6 @@ const path = require('path');
 const requireAll = require('require-all');
 const deepmerge = require('deepmerge')
 
-const eslint = requireAll({
-  dirname: path.resolve(__dirname, 'eslint'),
-})
-
 function getConfig(configs, rule, userConfig) {
   if (!configs[rule]) {
     throw new Error(`Rule '${rule}' not Support!`);
@@ -15,6 +11,19 @@ function getConfig(configs, rule, userConfig) {
 }
 
 // ESLint
+const eslint = requireAll({
+  dirname: path.resolve(__dirname, 'eslint'),
+})
+
 exports.getESLintConfig = function (rule, userConfig) {
   return getConfig(eslint, rule, userConfig);
+};
+
+// commitlint
+const commitlint = requireAll({
+  dirname: path.resolve(__dirname, 'commitlint'),
+});
+
+exports.getCommitlintConfig = function (rule, customConfig) {
+  return getConfig(commitlint, rule, customConfig);
 };
