8559e6a6f415c7ae3aaceaf822d0e54ed6580651
luhc228 <luhengchang228@gmail.com>
2021-12-09 15:20:56 +0800

    feat: update message

--- packages/projectlint/src/codemod/rules.ts
+++ packages/projectlint/src/codemod/rules.ts
@@ -4,22 +4,22 @@ const performanceRules: Record<string, Rule> = {
   'picture': {
     title: '指定图片宽度',
     'title_en': 'Specify image width',
-    message: '使用 Picture 组件时，通过 Style 属性显示指定图片宽度，可以在运行时加载合适尺寸的图片，从而有效减少图片加载体积',
-    'message_en': 'When using the Picture component, you can load images of the appropriate size at run time by displaying the specified image width through the Style property, which effectively reduces the image loading volume',
+    message: '使用 @ali/rax-picture 组件时，通过 Style 属性显式指定图片宽度，可以在运行时加载合适尺寸的图片，从而有效减少图片加载体积。',
+    'message_en': 'When using the @ali/rax-picture component, you can load images of the appropriate size at run time by displaying the specified image width through the Style property, which effectively reduces the image loading volume.',
     severity: Severity.warn,
     tags: ['performance'],
-    docs: 'https://web.npm.alibaba-inc.com/package/@ali/perf-codemod',
+    docs: 'https://rax.alibaba-inc.com/docs/components/picture',
     package: '@ali/perf-codemod',
     transform: 'src/transforms/picture.js'
   },
   'snapshot': {
     title: '开启快照',
     'title_en': 'Enable snapshot',
-    message: '开启快照（snapshot），可以有效提升页面的首屏可见时间。[详情](https://rax.alibaba-inc.com/docs/guide/snapshot)',
-    'message_en': 'Enabling the snapshot function improves the first screen view time.[Detail](https://rax.alibaba-inc.com/docs/guide/snapshot).',
+    message: '开启快照（snapshot），可以有效提升页面的首屏可见时间。',
+    'message_en': 'Enabling the snapshot function improves the first screen view time.',
     severity: Severity.warn,
     tags: ['performance'],
-    docs: 'https://web.npm.alibaba-inc.com/package/@ali/perf-codemod',
+    docs: 'https://rax.alibaba-inc.com/docs/guide/snapshot',
     package: '@ali/perf-codemod',
     transform: 'src/transforms/snapshot.js'
   }
@@ -29,8 +29,8 @@ const normalRules: Record<string, Rule> = {
     'plugin-rax-component-to-component': {
     title: 'Rax 组件工程升级',
     'title_en': 'Rax component project upgrade',
-    message: '从 plugin-rax-component 升级到 plugin-component',
-    'message_en': 'upgrade from plugin-rax-component to plugin-component',
+    message: '从 plugin-rax-component 升级到 plugin-component。',
+    'message_en': 'upgrade from plugin-rax-component to plugin-component.',
     severity: Severity.warn,
     tags: [],
     docs: 'https://rax.js.org/docs/guide/com-migration',
@@ -59,8 +59,8 @@ const normalRules: Record<string, Rule> = {
   'rax-app': {
     title: 'Rax 工程升级',
     'title_en': 'Rax project upgrade',
-    message: 'rax-app 从 2.x 升级到 3.x',
-    'message_en': 'rax-app upgrade from 2.x to 3.x',
+    message: 'rax-app 从 2.x 升级到 3.x。',
+    'message_en': 'rax-app upgrade from 2.x to 3.x.',
     severity: Severity.off,
     tags: [],
     docs: 'https://github.com/raxjs/rax-app/blob/master/packages/codemod/README.md',
