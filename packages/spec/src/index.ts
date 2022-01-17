3ede72a2cb5c66e05ede949b5a3c1f8db5e33efd
luhc228 <luhengchang228@gmail.com>
2022-01-17 11:13:47 +0800

    chore: add RuleKey type

--- packages/spec/src/index.ts
+++ packages/spec/src/index.ts
@@ -6,11 +6,13 @@ import type { UserConfig as CommitlintUserConfig } from '@commitlint/types';
 import type { Config as StylelintConfig } from 'stylelint';
 import type { Options as PrettierConfig } from 'prettier';
 
+type RuleKey = 'common' | 'common-ts' | 'react' | 'react-ts' | 'rax' | 'rax-ts';
+
 // ESLint
 const eslint = requireAll({
   dirname: path.resolve(__dirname, 'eslint'),
 });
-export function getESLintConfig(rule: string, userConfig: Linter.Config = {}) {
+export function getESLintConfig(rule: RuleKey, userConfig: Linter.Config = {}) {
   if (!eslint[rule]) {
     throw new Error(`Rule '${rule}' not Support!`);
   }
@@ -22,7 +24,7 @@ const commitlint = requireAll({
   dirname: path.resolve(__dirname, 'commitlint'),
 });
 // Note: rule param is to compatible with @iceworks/spec
-export function getCommitlintConfig(rule: string, userConfig: CommitlintUserConfig = {}) {
+export function getCommitlintConfig(rule: RuleKey, userConfig: CommitlintUserConfig = {}) {
   return deepmerge(commitlint.index.default, userConfig);
 }
 
@@ -31,7 +33,7 @@ const prettier = requireAll({
   dirname: path.resolve(__dirname, 'prettier'),
 });
 // Note: rule param is to compatible with @iceworks/spec
-export function getPrettierConfig(rule: string, userConfig: PrettierConfig = {}) {
+export function getPrettierConfig(rule: RuleKey, userConfig: PrettierConfig = {}) {
   return deepmerge(prettier.index.default, userConfig);
 }
 
@@ -40,6 +42,6 @@ const stylelint = requireAll({
   dirname: path.resolve(__dirname, 'stylelint'),
 });
 // Note: rule param is to compatible with @iceworks/spec
-export function getStylelintConfig(rule: string, userConfig: StylelintConfig = {}) {
+export function getStylelintConfig(rule: RuleKey, userConfig: StylelintConfig = {}) {
   return deepmerge(stylelint.index.default, userConfig);
 }
