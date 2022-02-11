18215d881295336ac00e79aea2f8be41021adfe0
luhc228 <luhengchang228@gmail.com>
2022-02-11 12:20:06 +0800

    chore: update eslint-diff

--- scripts/eslint-diff.ts
+++ scripts/eslint-diff.ts
@@ -5,32 +5,42 @@ import chalk from 'chalk';
 const GIT_DIFF = 'git diff --cached --diff-filter=ACMR --name-only';
 
 (async () => {
-  const linter = new ESLint({ extensions: ['.js', '.jsx', '.ts', '.tsx'] });
+  const linter = new ESLint();
 
   // file level
-  const fileList = await execSync(GIT_DIFF).toString().split('\n');
-
+  const fileList = execSync(GIT_DIFF).toString().split('\n');
   // ignore file which is not js/jsx/ts/tsx
-  const lintFileList = fileList.filter(
-    (file) => /\.(j|t)sx?$/g.test(file) && !linter.isPathIgnored(file),
-  );
+  const lintFilesList = (
+    await Promise.all(
+      fileList.map(async (file) => {
+        if (file && /\.(j|t)sx?$/g.test(file) && !(await linter.isPathIgnored(file))) {
+          return file;
+        }
+        return '';
+      }),
+    )
+  ).filter(Boolean);
 
-  if (!lintFileList.length) {
+  if (!lintFilesList.length) {
     console.log(chalk.green('no file should be lint.'));
     return;
   }
 
-  try {
-    console.log(chalk.green(lintFileList.join('\n')));
-    console.log();
-    console.log(chalk.green('above file should be lint.'));
-    const lintResults = await linter.lintFiles(lintFileList);
-    const Formatter = await linter.loadFormatter();
-    Formatter.format(lintResults);
-  } catch (error) {
+  console.log(chalk.green(lintFilesList.join('\n')));
+  console.log();
+  console.log(chalk.green('above file should be lint.'));
+
+  const lintResults = await linter.lintFiles(lintFilesList);
+  const Formatter = await linter.loadFormatter();
+  console.log(Formatter.format(lintResults));
+
+  const error = ESLint.getErrorResults(lintResults);
+  if (error.length > 0) {
     process.exit(1);
+  } else {
+    console.log(chalk.green('lint successfully.'));
   }
-})().catch((e) => {
-  console.trace(e);
+})().catch((error) => {
+  console.trace(error);
   process.exit(1);
 });
