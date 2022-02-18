74e5801289882e4360f43b65b278a43164d3d516
luhc228 <luhengchang228@gmail.com>
2022-02-18 17:03:46 +0800

    fix: get ESLint warning when use react component

--- packages/eslint-config/rules/react.js
+++ packages/eslint-config/rules/react.js
@@ -6,6 +6,12 @@ module.exports = {
      *  React 规则
      * @link https://github.com/yannickcr/eslint-plugin-react
      */
+    // 防止 React 被标记为未使用
+    'react/jsx-uses-react': 'error',
+
+    // 防止 React 变量被标记为未使用
+    'react/jsx-uses-vars': 'error',
+
     // JSX 语法使用两个空格缩进
     'react/jsx-indent': ['error', 2],
     'react/jsx-indent-props': ['error', 2],
